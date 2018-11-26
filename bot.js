bc62869c0ea33f63d95f3cca4ea1ccbc5c14f26f
texct999 <45326466+texct999@users.noreply.github.com>
2018-11-27 00:36:21 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,80 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'السلام عليكم ') {
+    msg.reply('وعليكم السلام ورحمة الله وبركاتة');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dt39RA.EIcBD2hyWazgtE8uCIA0Vfr6uFo');
+
+client.on('message', msg => {
+    if (msg.content === 'هلا ') {
+      msg.reply('اهلين');
+    }
+  });
+  
+  client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dt39RA.EIcBD2hyWazgtE8uCIA0Vfr6uFo');
+
+  client.on('message', msg => {
+    if (msg.content === 'باك') {
+      msg.reply('ولكم منور يا جميل');
+    }
+  });
+  
+  client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dt39RA.EIcBD2hyWazgtE8uCIA0Vfr6uFo');
+
+  client.on('message', msg => {
+    if (msg.content === 'الشعار') {
+      msg.reply('ƒreeƒier | هنا اسمك');
+    }
+  });
+  
+  client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dt39RA.EIcBD2hyWazgtE8uCIA0Vfr6uFo');
+
+  
+  client.on('message', msg => {
+    if (msg.content === 'لا اله الا الله') {
+      msg.reply('محمد رسول الله');
+    }
+  });
+  
+  client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dt39RA.EIcBD2hyWazgtE8uCIA0Vfr6uFo');
+
+  
+  client.on('message', msg => {
+    if (msg.content === 'ماما') {
+      msg.reply(':kissing_closed_eyes: ولدى حبيبى عطنى بوسة ');
+    }
+  });
+  
+  client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dt39RA.EIcBD2hyWazgtE8uCIA0Vfr6uFo');
+
+  client.on('message', msg => {
+    if (msg.content === 'ام الدنيا ') {
+      msg.reply(' مصر :flag_eg:');
+    }
+  });
+  
+  client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dt39RA.EIcBD2hyWazgtE8uCIA0Vfr6uFo');
+
+  client.on('message', msg => {
+    if (msg.content === 'انا احب pro bot') {
+      msg.reply('ورع ما تحبنى ما ابى اكلمك ورع خب توف عليك');
+    }
+  });
+  
+  client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dt39RA.EIcBD2hyWazgtE8uCIA0Vfr6uFo');
+
+  client.on('message', msg => {
+    if (msg.content === 'ههه') {
+      msg.reply(':relaxed: :relaxed: :relaxed: ما فى اشى يضحك ');
+    }
+  });
+  
+  client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dt39RA.EIcBD2hyWazgtE8uCIA0Vfr6uFo');
